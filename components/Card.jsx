d28c79a1af46468197d6d7c96578bf40f6511558
Suzan M <suzanmanasreh@gmail.com>
2022-02-22 13:00:23 -0500

    add calendar icon

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { FaCalendarAlt } from "react-icons/fa";
+import Link from "next/link";
+
 {
   /* 
-  TODO: make cards link to conference website
-  TODO: add calendar icon and make it add conference date to calendar 
+  TODO: make it so that when user clicks calendar button they can add conference date to their calendar 
+  TODO: or install add to calendar package and use that
   */
 }
+
 const Card = ({ conference }) => {
   return (
-    <div className="border-2 border-stone-800 bg-white-20 p-2">
-      <div className="text-base text-stone-800 font-semibold">
-        {conference.name}
+    <div className="border-2 border-stone-800 bg-white-20 p-2 hover:bg-slate-200">
+      <div className="flex justify-between text-base text-stone-800 font-semibold">
+        <Link href={conference.website} passHref>
+          <a>
+            <div className="hover:text-yellow-20">{conference.name}</div>
+          </a>
+        </Link>
+        <div className="shrink-0 p-1 hover:text-blue-950">
+          <FaCalendarAlt size={20} />
+        </div>
       </div>
       <div className="font-medium text-stone-800 text-sm">
         {conference.location}
